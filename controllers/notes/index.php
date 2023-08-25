26fba14dc014dcd304c553373dca0c7999638920
Abolfazl Ghaffari <abolfazl.ghaffari@sarveno.com>
2023-08-25 12:32:18 +0330

    Resourcefull naming convention

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -7,4 +7,4 @@ $db = new Database($config['database']);
 
 $notes = $db->query('SELECT * FROM notes WHERE user_id = :user', ['user' => 1,])->get();
 
-require 'views/notes.view.php';
+require 'views/notes/index.view.php';
