96709b4d63e75a20a6d2ea457ccc139870cc6b71
Abolfazl Ghaffari <abolfazl.ghaffari@sarveno.com>
2023-08-27 20:26:39 +0330

    Automatically Redirect Back Upon Failed Validation

--- core/Validator.php
+++ core/Validator.php
@@ -3,15 +3,15 @@
 namespace Core;
 class Validator
 {
+        public static function email($email)
+        {
+                return filter_var($email, FILTER_VALIDATE_EMAIL);
+        }
+
         public static function string($value, $min = 1, $max = INF)
         {
                 $value = trim($value);
 
                 return strlen($value) >= $min && strlen($value) <= $max;
-        }
-
-        public static function email($email)
-        {
-                return filter_var($email, FILTER_VALIDATE_EMAIL);
         }
 }
